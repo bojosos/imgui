4242d0d3f15e3208bdd3031862595c7d3d5be863
bojosos <ivanov6@abv.bg>
2021-01-21 21:53:47 +0200

    to docking branch

---
+++ bin-int/Release-linux-x86_64/imgui/Linux64/imgui_demo.d
@@ -0,0 +1,6 @@
+bin-int/Release-linux-x86_64/imgui/Linux64/imgui_demo.o: imgui_demo.cpp \
+  imgui.h imconfig.h
+
+imgui.h:
+
+imconfig.h:
---
+++ bin-int/Release-linux-x86_64/imgui/Linux64/imgui_stdlib.d
@@ -0,0 +1,8 @@
+bin-int/Release-linux-x86_64/imgui/Linux64/imgui_stdlib.o: \
+  misc/cpp/imgui_stdlib.cpp imgui.h imconfig.h misc/cpp/imgui_stdlib.h
+
+imgui.h:
+
+imconfig.h:
+
+misc/cpp/imgui_stdlib.h:
