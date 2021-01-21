846b04189435cb6ff960b08c491ca55f6553504a
bojosos <ivanov6@abv.bg>
2021-01-21 21:53:47 +0200

    to docking branch

---
+++ bin-int/Release-linux-x86_64/imgui/Linux64/imgui.d
@@ -0,0 +1,10 @@
+bin-int/Release-linux-x86_64/imgui/Linux64/imgui.o: imgui.cpp imgui.h \
+  imconfig.h imgui_internal.h imstb_textedit.h
+
+imgui.h:
+
+imconfig.h:
+
+imgui_internal.h:
+
+imstb_textedit.h:
---
+++ bin-int/Release-linux-x86_64/imgui/Linux64/imgui_widgets.d
@@ -0,0 +1,10 @@
+bin-int/Release-linux-x86_64/imgui/Linux64/imgui_widgets.o: \
+  imgui_widgets.cpp imgui.h imconfig.h imgui_internal.h imstb_textedit.h
+
+imgui.h:
+
+imconfig.h:
+
+imgui_internal.h:
+
+imstb_textedit.h:
